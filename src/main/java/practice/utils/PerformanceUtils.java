7f3ac0945bbdefafb2566a69e74733bc0fa447f6
big-newbie <liutingfang_01@foxmail.com>
2016-06-17 23:12:59 +0800

    long time no see

--- src/main/java/practice/utils/PerformanceUtils.java
+++ src/main/java/practice/utils/PerformanceUtils.java
@@ -1,25 +1,36 @@
 package practice.utils;
 
+import java.util.Random;
+
 /**
  * Created by tingfang.liu on 2016/4/16.
  */
 public class PerformanceUtils {
 
-    public static void test(Processer processer) {
+    public static void test(String task, Processer processer) {
 
         long s = System.currentTimeMillis();
-        processer.process();
-        long s2 = System.currentTimeMillis();
-        if (s2 - s < 10) {
-            for (int i = 0; i < 10000; i++) {
-                processer.process();
-            }
+        long s2 = s;
+        long count = 0;
+        while (s2 - s < 10000) {
+            processer.process();
+            s2 = System.currentTimeMillis();
+            count++;
         }
-        s = System.currentTimeMillis();
-        System.out.println("cost time millis : " + Math.abs(s2 - s));
+        System.out.println(task + " run " + count + " times. avg " + (s2 - s) / count + " mills second");
     }
 
     public interface Processer {
         void process();
+    }
+
+    public static void main(String[] args) {
+        PerformanceUtils.test("test task", () -> {
+            Random random = new Random();
+            try {
+                Thread.sleep(random.nextInt(1000));
+            } catch (InterruptedException e) {
+            }
+        });
     }
 }
